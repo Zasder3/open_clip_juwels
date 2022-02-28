8df6496305bc1522b023db3f8eb87201f5ded017
Cade Gordon <cadegord@berkeley.edu>
2022-02-28 11:21:02 -0800

    224 run

--- src/training/main.py
+++ src/training/main.py
@@ -78,8 +78,8 @@ def main_worker(gpu, ngpus_per_node, log_queue, args):
             is_train=True)
         # preprocess_train, preprocess_val = _transform(336, is_train=True), _transform(336, is_train=False)
         # model.visual.positional_embedding = torch.nn.Parameter(1024**-0.5 * torch.randn((336 // 14) ** 2 + 1, 1024))
-        # model.visual.transformer.gradient_checkpointing = args.gradient_checkpointing
-        # model.transformer.gradient_checkpointing = args.gradient_checkpointing
+        model.visual.transformer.gradient_checkpointing = args.gradient_checkpointing
+        model.transformer.gradient_checkpointing = args.gradient_checkpointing
     else:
         model_config_file = Path(__file__).parent / f"model_configs/{args.model.replace('/', '-')}.json"
         print('Loading model from', model_config_file)
