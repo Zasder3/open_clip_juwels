469d73c07350c16834f76a8c538d5f37f4723374
Cade Gordon <cadegord@berkeley.edu>
2022-03-01 16:05:10 -0800

    log grad norm and clip grads

--- src/training/train.py
+++ src/training/train.py
@@ -113,19 +113,15 @@ def train(model, data, epoch, optimizer, scaler, scheduler, args, tb_writer=None
         if args.precision == "amp":
             with autocast():
                 total_loss = get_loss(model, images, texts, loss_img, loss_txt, args)
-
-                # Log gradient norms.
-                if args.distributed and args.aggregate:
-                    world_size = dist.get_world_size()
-                    rank = dist.get_rank()
-                    if rank == 0:
-                        grads = []
-                        for param in m.parameters():
-                            grads.append(param.grad.detach().cpu().numpy())
-                        grads = np.concatenate([np.sum(g**2)**0.5 for g in grads], axis=0)
-                        wandb.log({"grad_norm": grads.mean(), "step": epoch * num_batches_per_epoch + i})
-                nn.utils.clip_grad_norm_(model.parameters(), 0.25)
                 scaler.scale(total_loss).backward()
+                rank = dist.get_rank()
+                if rank == 0:
+                    grads = []
+                    for param in m.parameters():
+                        grads.append(param.grad.detach().cpu().numpy())
+                    grads = np.concatenate([np.sum(g**2)**0.5 for g in grads], axis=0)
+                    wandb.log({"grad_norm": grads.mean(), "step": epoch * num_batches_per_epoch + i})
+                nn.utils.clip_grad_norm_(model.parameters(), 0.25)
                 scaler.step(optimizer)
             scaler.update()
 
